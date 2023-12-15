c67ceb07dad5969869b67bc07cfea84158ef76a2
RehanGith <chrehan1007@gmail.com>
2023-12-15 18:54:39 +0500

    Add project files.

---
+++ Inheritance Using Polymorphism/main.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+const int MAX{ 100 };
+class Person {
+protected:
+	string name;
+	string cnic;
+public:
+	Person(string n = "", string c = "");
+	virtual void setter();
+	virtual void getter();
+	virtual void isoutstanding() = 0;
+	virtual ~Person();
+};
+Person::Person(string n, string c)
+	: name(n), cnic(c) {
+
+}
+
+void Person::setter() {
+	cin.ignore();
+	cout << "Enter name: ";
+	getline(cin, name);
+	cout << "Enter cnic: ";
+	getline(cin, cnic);
+}
+void Person::getter() {
+	cout << "Name: " << name << "  cnic: " << cnic << endl;
+}
+Person::~Person() {
+
+}
+class Student : virtual public Person {
+	string degree_program;
+	float cgpa;
+public:
+	Student(string n = "", string c = "", string d = "", float g = 0.0);
+	void setter();
+	void getter();
+	void isoutstanding() {
+		if (cgpa > 3.0) {
+			cout << "***Outstanding***" << endl;
+		}
+	}
+};
+Student::Student(string n, string c, string d, float g)
+	: Person(n, c), degree_program(d), cgpa(g) {
+
+}
+void Student::setter() {
+	Person::setter();
+	cin.ignore();
+	cout << "Enter degree Program: ";
+	getline(cin, degree_program);
+	cout << "Enter cgpa: ";
+	cin >> cgpa;
+}
+void Student::getter() {
+	cout << "***Student***" << endl;
+	Person::getter();
+	cout << "Degree Program: " << degree_program << " cgpa: " << cgpa << endl;
+}
+
+class Teacher : virtual public Person {
+	string deperatment;
+	int no_of_publication;
+public:
+	Teacher(string n = "", string c = "", string d = "", int no = 0);
+	void setter();
+	void getter();
+	void isoutstanding() {
+		if (no_of_publication > 10) {
+			cout << "***Outstanding***" << endl;
+		}
+	}
+};
+Teacher::Teacher(string n, string c, string d, int no)
+	: Person(n, c), deperatment(d), no_of_publication(no) {
+
+}
+void Teacher::setter() {
+	Person::setter();
+	cin.ignore();
+	cout << "Enter Department: ";
+	getline(cin, deperatment);
+	cout << "NO of Publication: ";
+	cin >> no_of_publication;
+}
+void Teacher::getter() {
+	cout << "***Teacher***" << endl;
+	Person::getter();
+	cout << "Department: " << deperatment << "  no of Publication: " << no_of_publication << endl;
+}
+
+
+int main() {
+	int choice{}, n{}, nt{}, ns{};
+	Person* arr[MAX]{};
+	while (true) {
+		cout << "Menu\n"
+			<< "1: Create Teacher\n"
+			<< "2: Create Student\n"
+			<< "3: Display All\n"
+			<< "4: Dispaly number\n"
+			<< "5: Exit" << endl;
+		cout << "Which option you wants to choose: ";
+		cin >> choice;
+		switch (choice) {
+		case 1: // Create a Teacher
+			arr[n] = new Teacher;
+			arr[n++]->setter();
+			nt++;
+			break;
+		case 2: //Create a Student
+			arr[n] = new Student;
+			arr[n++]->setter();
+			ns++;
+			break;
+		case 3:  //Display all
+			for (int i = 0; i < n; i++) {
+				arr[i]->getter();
+				arr[i]->isoutstanding();
+			}
+			break;
+		case 4:
+			cout << "Number of teachers: " << nt << endl;
+			cout << "Number of Student: " << ns << endl;
+			break;
+		case 5:
+			for (int i = 0; i < n; i++) {
+				delete arr[i];
+			}
+			return 0;
+		default:
+			cout << "You enter wrong choice" << endl;
+			break;
+		}
+	}
+	return 0;
+}
